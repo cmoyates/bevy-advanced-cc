6d79a935729bafc70668ffecb91458ffd05f4fe4
cmoyates <cmoyates@gmail.com>
2025-12-23 14:12:28 -0330

    A bunch of old code

---
+++ src/collisions.rs
@@ -0,0 +1,206 @@
+use bevy::{
+    app::{App, Plugin, Update},
+    ecs::{
+        schedule::IntoSystemConfigs,
+        system::{Query, Res},
+    },
+    gizmos::gizmos::Gizmos,
+    math::{Vec2, Vec3Swizzles},
+    render::color::Color,
+    transform::components::Transform,
+};
+
+use crate::{s_movement, Level, Physics, Player, MAX_GROUNDED_TIMER, MAX_WALLED_TIMER};
+
+pub struct CollisionPlugin;
+
+impl Plugin for CollisionPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Update, s_collision.after(s_movement));
+    }
+}
+
+pub fn s_collision(
+    mut player_query: Query<(&mut Transform, &mut Physics, &mut Player)>,
+    level: Res<Level>,
+    mut gizmos: Gizmos,
+) {
+    if let Ok((mut player_transform, mut player_physics, mut player_data)) =
+        player_query.get_single_mut()
+    {
+        let mut adjustment = Vec2::ZERO;
+
+        let mut new_player_normal = Vec2::ZERO;
+
+        for polygon in &level.polygons {
+            let mut intersect_counter = 0;
+            let mut colliding_with_polygon = false;
+
+            let mut intersection_points: Vec<Vec2> = Vec::new();
+
+            for i in 1..polygon.points.len() {
+                let start = polygon.points[i - 1];
+                let end = polygon.points[i];
+
+                let intersection = line_intersect(
+                    start,
+                    end,
+                    player_transform.translation.xy(),
+                    player_transform.translation.xy() + Vec2::new(2.0, 1.0) * 10000.0,
+                );
+
+                if intersection.is_some() {
+                    intersection_points.push(intersection.unwrap());
+                    intersect_counter += 1;
+                }
+
+                let previous_side_of_line =
+                    side_of_line_detection(start, end, player_physics.prev_position);
+
+                if previous_side_of_line != polygon.collision_side {
+                    continue;
+                }
+
+                let (distance_sq, projection) = find_projection(
+                    start,
+                    end,
+                    player_transform.translation.xy(),
+                    player_physics.radius,
+                );
+
+                let colliding_with_line = distance_sq <= player_physics.radius.powi(2);
+                colliding_with_polygon = colliding_with_polygon || colliding_with_line;
+
+                let touching_line = distance_sq <= (player_physics.radius + 0.5).powi(2);
+
+                if touching_line {
+                    let normal_dir =
+                        (player_transform.translation.xy() - projection).normalize_or_zero();
+
+                    // If the line is not above the player
+                    if normal_dir.y >= -0.01 {
+                        gizmos.line_2d(
+                            player_transform.translation.xy(),
+                            player_transform.translation.xy() - normal_dir * 12.0,
+                            Color::WHITE,
+                        );
+
+                        // Add the normal dir to the players new normal
+                        new_player_normal -= normal_dir;
+
+                        // If the player is on a wall
+                        if normal_dir.x.abs() >= 0.8 {
+                            player_data.walled_timer = MAX_WALLED_TIMER * normal_dir.x as i32;
+                            player_data.has_wall_jumped = false;
+                        }
+
+                        // If the player is on the ground
+                        if normal_dir.y > 0.01 {
+                            player_data.grounded_timer = MAX_GROUNDED_TIMER;
+                            player_data.walled_timer = 0;
+                            player_data.has_wall_jumped = false;
+                        }
+                    }
+                }
+
+                if colliding_with_line {
+                    let mut delta =
+                        (player_transform.translation.xy() - projection).normalize_or_zero();
+
+                    if delta.y < -0.01 {
+                        // println!("Hit ceiling");
+                        // dbg!(delta);
+                        player_physics.velocity.y = 0.0;
+                    }
+
+                    delta *= player_physics.radius - distance_sq.sqrt();
+
+                    if delta.x.abs() > adjustment.x.abs() {
+                        adjustment.x = delta.x;
+                    }
+                    if delta.y.abs() > adjustment.y.abs() {
+                        adjustment.y = delta.y;
+                    }
+                }
+            }
+            if colliding_with_polygon && intersect_counter % 2 == 1 {
+                player_transform.translation = player_physics.prev_position.extend(0.0);
+            }
+        }
+
+        // Update the players normal
+        new_player_normal = new_player_normal.normalize_or_zero();
+        player_physics.normal = new_player_normal;
+
+        // Remove the players velocity in the direction of the normal
+        let velocity_adjustment =
+            player_physics.velocity.dot(new_player_normal) * new_player_normal;
+
+        // dbg!(player_physics.normal);
+
+        player_physics.velocity -= velocity_adjustment;
+
+        // Update the players position
+        player_transform.translation += adjustment.extend(0.0);
+    }
+}
+
+pub fn find_projection(start: Vec2, end: Vec2, point: Vec2, radius: f32) -> (f32, Vec2) {
+    let point_vec = point - start;
+    let line_vec = end - start;
+
+    let line_vec_normalized = line_vec.normalize();
+
+    let dot = point_vec.dot(line_vec_normalized);
+
+    let projection_point = line_vec_normalized * dot + start;
+
+    if dot < 0.0 {
+        return (point_vec.length_squared() + radius * 2.0, projection_point);
+    }
+
+    if dot.powi(2) > (end - start).length_squared() {
+        return (
+            (point - end).length_squared() + radius * 2.0,
+            projection_point,
+        );
+    }
+
+    let dist = (point - projection_point).length_squared();
+
+    return (dist, projection_point);
+}
+
+pub fn side_of_line_detection(line_start: Vec2, line_end: Vec2, point: Vec2) -> f32 {
+    let determinant = (line_end.x - line_start.x) * (point.y - line_start.y)
+        - (line_end.y - line_start.y) * (point.x - line_start.x);
+
+    return determinant.signum();
+}
+
+pub fn line_intersect(
+    line_1_start: Vec2,
+    line_1_end: Vec2,
+    line_2_start: Vec2,
+    line_2_end: Vec2,
+) -> Option<Vec2> {
+    let line_1 = line_1_end - line_1_start;
+    let line_2 = line_2_end - line_2_start;
+    let r_cross_s = cross_product(line_1, line_2);
+    let a_to_c = line_2_start - line_1_start;
+    let t = cross_product(a_to_c, line_2) / r_cross_s;
+    let u = cross_product(a_to_c, line_1) / r_cross_s;
+
+    if t >= 0.0 && t <= 1.0 && u >= 0.0 && u <= 1.0 {
+        Some(Vec2::new(
+            line_1_start.x + t * line_1.x,
+            line_1_start.y + t * line_1.y,
+        ))
+    } else {
+        None
+    }
+}
+
+pub fn cross_product(a: Vec2, b: Vec2) -> f32 {
+    a.x * b.y - a.y * b.x
+}
---
+++ src/main.rs
@@ -0,0 +1,310 @@
+mod collisions;
+mod level;
+
+use std::{thread::sleep, time::Duration};
+
+use ::bevy::prelude::*;
+use bevy::{app::AppExit, window::PresentMode};
+use collisions::{s_collision, CollisionPlugin};
+use level::{generate_level_polygons, Polygon};
+
+const FRAMERATE: u32 = 60;
+const FRAME_DURATION_SECS: f32 = 1.0 / FRAMERATE as f32;
+
+fn main() {
+    App::new()
+        .insert_resource(ClearColor(Color::rgb(0.0, 0.0, 0.0)))
+        .insert_resource(InputDir { dir: Vec2::ZERO })
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Advanced Character Controller".to_string(),
+                present_mode: PresentMode::AutoNoVsync,
+                fit_canvas_to_parent: true,
+                ..default()
+            }),
+            ..default()
+        }))
+        .add_plugins(CollisionPlugin)
+        // Startup systems
+        .add_systems(Startup, s_init)
+        // Update systems
+        .add_systems(Update, s_input)
+        .add_systems(Update, s_movement.after(s_input))
+        .add_systems(Update, s_timers.after(s_collision))
+        .add_systems(Update, s_render.after(s_timers))
+        .add_systems(Update, s_wait_for_next_frame.after(s_render))
+        .run();
+}
+
+#[derive(Resource)]
+pub struct Level {
+    pub polygons: Vec<Polygon>,
+}
+
+#[derive(Resource)]
+pub struct InputDir {
+    pub dir: Vec2,
+}
+
+pub const PLAYER_MAX_SPEED: f32 = 5.0;
+pub const PLAYER_ACCELERATION_SCALERS: (f32, f32) = (0.2, 0.4);
+
+pub const MAX_JUMP_TIMER: i32 = 10;
+pub const MAX_GROUNDED_TIMER: i32 = 10;
+pub const MAX_WALLED_TIMER: i32 = 10;
+
+#[derive(Component)]
+pub struct Player {
+    jump_timer: i32,
+    grounded_timer: i32,
+    walled_timer: i32,
+    has_wall_jumped: bool,
+}
+
+#[derive(Component)]
+pub struct Physics {
+    pub prev_position: Vec2,
+    pub velocity: Vec2,
+    pub acceleration: Vec2,
+    pub radius: f32,
+    pub normal: Vec2,
+}
+
+/// Initial setup system
+pub fn s_init(mut commands: Commands) {
+    // Spawn camera
+    commands.spawn(Camera2dBundle::default());
+
+    // Spawn player
+    commands.spawn((
+        Transform::from_translation(Vec3::new(0.0, -50.0, 0.0)),
+        Physics {
+            prev_position: Vec2::ZERO,
+            velocity: Vec2::ZERO,
+            acceleration: Vec2::ZERO,
+            radius: 12.0,
+            normal: Vec2::ZERO,
+        },
+        Player {
+            jump_timer: 0,
+            grounded_timer: 0,
+            walled_timer: 0,
+            has_wall_jumped: false,
+        },
+    ));
+
+    // Init level
+    {
+        let grid_size = 32.0;
+
+        let level_polygons = generate_level_polygons(grid_size);
+
+        commands.insert_resource(Level {
+            polygons: level_polygons,
+        });
+    }
+}
+
+/// Input system
+pub fn s_input(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut exit: EventWriter<AppExit>,
+    mut input_dir: ResMut<InputDir>,
+    mut player_query: Query<(&mut Player, &mut Physics)>,
+) {
+    if let Ok((mut player_data, mut player_physics)) = player_query.get_single_mut() {
+        let mut direction = Vec2::ZERO;
+
+        // Escape to exit
+        if keyboard_input.just_pressed(KeyCode::Escape) {
+            exit.send(AppExit);
+        }
+
+        // Arrow keys to move
+        if keyboard_input.pressed(KeyCode::Up) {
+            direction.y += 1.0;
+        }
+        if keyboard_input.pressed(KeyCode::Down) {
+            direction.y -= 1.0;
+        }
+        if keyboard_input.pressed(KeyCode::Left) {
+            direction.x -= 1.0;
+        }
+        if keyboard_input.pressed(KeyCode::Right) {
+            direction.x += 1.0;
+        }
+
+        // Space to jump
+        if keyboard_input.just_pressed(KeyCode::Space) {
+            player_data.jump_timer = MAX_JUMP_TIMER;
+        }
+
+        if keyboard_input.just_released(KeyCode::Space) {
+            if player_physics.velocity.y > 0.0 {
+                player_physics.velocity.y /= 3.0;
+            }
+        }
+
+        // Normalize direction
+        direction = direction.normalize_or_zero();
+
+        // Set direction resource
+        input_dir.dir = direction;
+    }
+}
+
+/// Movement system
+pub fn s_movement(
+    mut player_query: Query<(&mut Transform, &mut Physics, &mut Player)>,
+    mut input_dir: ResMut<InputDir>,
+) {
+    if let Ok((mut player_transform, mut player_physics, mut player_data)) =
+        player_query.get_single_mut()
+    {
+        let player_falling = player_physics.normal.length_squared() == 0.0;
+        let no_input = input_dir.dir.length_squared() == 0.0;
+
+        // Rotate input according to the normal
+        if !no_input && !player_falling && input_dir.dir.dot(player_physics.normal).abs() < 0.8 {
+            let mut new_input_dir = Vec2::new(player_physics.normal.y, -player_physics.normal.x);
+
+            if new_input_dir.dot(input_dir.dir) < 0.0 {
+                new_input_dir *= -1.0;
+            }
+
+            input_dir.dir = new_input_dir;
+        }
+
+        // If the player is on a wall and is trying to move away from it
+        let player_move_off_wall = player_physics.normal.x.abs() >= 0.8
+            && input_dir.dir.x.abs() >= 0.8
+            && player_physics.normal.x.signum() != input_dir.dir.x.signum();
+
+        // Acceleration
+        {
+            // Apply acceleration
+            player_physics.acceleration = (input_dir.dir * PLAYER_MAX_SPEED
+                - player_physics.velocity)
+                * if no_input {
+                    // Deacceleration
+                    PLAYER_ACCELERATION_SCALERS.1
+                } else {
+                    // Acceleration
+                    PLAYER_ACCELERATION_SCALERS.0
+                };
+
+            // Wall jump physics
+            player_physics.acceleration *= if player_data.has_wall_jumped {
+                0.5
+            } else {
+                1.0
+            };
+
+            // If the player is falling
+            if player_falling {
+                // Ignore any other acceleration in the y direction
+                player_physics.acceleration.y = 0.0;
+            }
+            // Unless the player is on a wall and is trying to move away from it
+            if !player_move_off_wall {
+                // Remove the acceleration in the direction of the normal
+                let acceleration_adjustment =
+                    player_physics.normal * player_physics.acceleration.dot(player_physics.normal);
+                player_physics.acceleration -= acceleration_adjustment;
+            }
+        }
+
+        // Gravity
+        {
+            if player_move_off_wall || player_falling {
+                // Gravity goes down
+                player_physics.acceleration.y = -0.5;
+            } else {
+                // Gravity goes towards the normal
+                let gravity_normal_dir = player_physics.normal * 0.5;
+                player_physics.acceleration += gravity_normal_dir;
+            }
+        }
+
+        // Jumping
+        {
+            // If the player is trying to jump
+            if player_data.jump_timer > 0 {
+                // If on the ground
+                if player_data.grounded_timer > 0 {
+                    // Jump
+                    player_physics.velocity.y = 9.0;
+                    player_data.jump_timer = 0;
+                    player_data.grounded_timer = 0;
+                }
+                // If on a wall
+                else if player_data.walled_timer != 0 {
+                    // Wall jump
+                    player_physics.velocity.y = 4.5;
+                    player_physics.velocity.x = player_data.walled_timer.signum() as f32 * 7.8;
+                    player_data.jump_timer = 0;
+                    player_data.walled_timer = 0;
+                    player_data.has_wall_jumped = true;
+                }
+            }
+        }
+
+        // Update physics
+        player_physics.prev_position = player_transform.translation.xy();
+        let new_velocity = player_physics.velocity + player_physics.acceleration;
+        player_physics.velocity = new_velocity;
+        player_transform.translation.x += player_physics.velocity.x;
+        player_transform.translation.y += player_physics.velocity.y;
+    }
+}
+
+/// Render system
+pub fn s_render(
+    mut gizmos: Gizmos,
+    mut player_query: Query<(&mut Transform, &mut Physics), With<Player>>,
+    level: Res<Level>,
+) {
+    if let Ok((player_transform, player_physics)) = player_query.get_single_mut() {
+        // Draw player
+        gizmos.circle_2d(
+            player_transform.translation.xy(),
+            player_physics.radius,
+            Color::WHITE,
+        );
+
+        // Draw level
+        for polygon in &level.polygons {
+            gizmos.linestrip_2d(
+                polygon.points.iter().cloned().collect::<Vec<Vec2>>(),
+                polygon.color,
+            );
+        }
+    }
+}
+
+pub fn s_timers(mut player_query: Query<&mut Player>) {
+    if let Ok(mut player_data) = player_query.get_single_mut() {
+        if player_data.jump_timer > 0 {
+            player_data.jump_timer -= 1;
+        }
+        if player_data.grounded_timer > 0 {
+            player_data.grounded_timer -= 1;
+        }
+        if player_data.walled_timer.abs() > 0 {
+            player_data.walled_timer -= player_data.walled_timer.signum();
+        }
+    }
+}
+
+/// Framerate capping system
+pub fn s_wait_for_next_frame(time: Res<Time>) {
+    // If not running in wasm
+    #[cfg(not(target_arch = "wasm32"))]
+    {
+        // Sleep to cap framerate
+        let time_to_sleep = FRAME_DURATION_SECS - time.delta().as_secs_f32();
+        if time_to_sleep > 0.0 {
+            sleep(Duration::from_secs_f32(time_to_sleep));
+        }
+    }
+}
